e57710af2f298ee95ae906ee79a9f5c04c936f52
git <elvisramirez999@gmail.com>
2023-06-09 05:09:01 +0000

    Added Components

---
+++ src/components/AddWorkout.js
@@ -0,0 +1,44 @@
+import React, { useState} from "react";
+
+
+const AddWorkout = () => {
+    const [exercise, setExercise] = useState('');
+    const [sets, setSets] = useState("");
+    const [reps, setReps] = useState("");
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (exercise.trim() === '' || sets.trim() === '' || reps.trim() === '') {
+            return;
+        }
+
+        const newWorkout = {
+            exercise,
+            sets: parseInt(sets),
+            reps: parseInt(reps)
+        };
+        // Add the workout to the workout history 
+        setExercise('');
+        setSets('');
+        setReps('');
+    }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <label>
+                Exercise:
+                <input type="text" value={exercise} onChange={e => setExercise(e.target.value)} />
+            </label>
+            <label>
+                Sets:
+                <input type="text" value={sets} onChange={e => setSets(e.target.value)} />
+            </label>
+            <label>
+                Reps:
+                <input type="text" value={reps} onChange={e => setReps(e.target.value)} />
+            </label>
+            <button type="submit">Add Workout</button>
+        </form>
+    )
+};
+
+export default AddWorkout;
